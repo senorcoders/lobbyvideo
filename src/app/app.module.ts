b21ea321dd67f7e543989d2dbe27f37eafa86d72
Dayana <dayana@senorcoders.com>
2020-01-15 14:44:38 -0600

    Homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AppMenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
